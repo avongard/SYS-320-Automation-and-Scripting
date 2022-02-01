721681da671f58c0a82967f308cc2b144a538167
BuildTools <unconfigured@null.spigotmc.org>
2022-02-01 17:08:23 -0500

    Week 2 Homework Complete

---
+++ Week 2/homework/apacheLog.py
@@ -0,0 +1,47 @@
+import syslogCheck
+import importlib
+importlib.reload(syslogCheck)
+
+# Proxy
+def proxy_events(filename, service, term):
+    # Call syslogCheck and return the results
+    is_found = syslogCheck._syslog(filename, service, term)
+
+    # Found list
+    found = []
+
+    # Loop through results
+    for eachFound in is_found:
+        # print(eachFound)
+        # Split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split value to the found list
+        found.append(sp_results[-2])
+
+    hosts = set(found)
+
+    for eachValue in hosts:
+        print(eachValue)
+
+# Bytes
+def byte_events(filename, service, term):
+    # Call syslogCheck and return the results
+    is_found = syslogCheck._syslog(filename, service, term)
+
+    # Found list
+    found = []
+
+    # Loop through results
+    for eachFound in is_found:
+        # print(eachFound)
+        # Split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split value to the found list
+        found.append("Bytes Sent: " + sp_results[4] + "      Bytes Recieved: " + sp_results[7])
+
+    hosts = set(found)
+
+    for eachValue in hosts:
+        print(eachValue)
